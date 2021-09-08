757ea4ef937e769d39d37b9fd041cc921b828978
System Administrator <root@Pavels-MacBook-Pro.local>
2021-09-08 21:25:25 +0300

    Implemented Post Delete functionality

--- src/containers/PostDetail.js
+++ src/containers/PostDetail.js
@@ -1,10 +1,77 @@
-import React from 'react'
-import {Container, Header, Image} from 'semantic-ui-react'
+import React, { useState } from 'react'
+import {Button, Container, Divider, Header, Image, Icon, Modal} from 'semantic-ui-react'
+import axios from "axios"
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { useParams } from 'react-router-dom';
 import { api } from '../api';
-import { useFetch } from "../helpers";
+import { useFetch, history } from "../helpers";
+
+const DeleteModal = ({title, postSlug, thumbnail}) => {
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  function handleSubmit() {
+    setLoading(true);
+    axios
+      .delete(api.posts.delete(postSlug), {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "Authorization": "Token aeaaaf8d52e2c7a986d3b25a4b379cca1c51b27b"
+        }
+      })
+      .then(res => {
+        // console.log(res)
+        setLoading(false);
+        history.push('/')
+        // redirect back to the post list
+      })
+      .catch(err => {
+        setLoading(false);
+        setError(err.message || err)
+      })
+  }
+
+  // state = { modalOpen: false }
+  // handleOpen = () => this.setState({ modalOpen: true })
+  // handleClose = () => this.setState({ modalOpen: false })
+  const [open, toggle] = useState(false);
+
+  return (
+    <Modal
+      trigger={<Button secondary floated="right" onClick={() => toggle(true)}>Delete post</Button>}
+      open={open}
+      onClose={() => toggle(false)}
+      size='small'
+    >
+      <Modal.Header>Delete post</Modal.Header>
+      <Modal.Content image>
+        <Image size='medium' src={thumbnail} wrapped />
+        <Modal.Description>
+          <Header>{title}</Header>
+          {error && <Message negative message={error} />}
+          <p>
+            Are you sure you want to delete this post?
+          </p>
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color='black' onClick={() => toggle(false)}>
+          No
+        </Button>
+        <Button
+          positive
+          icon='checkmark'
+          labelPosition='right'
+          content="Confirm delete"
+          onClick={handleSubmit}
+          loading={loading}
+          disabled={loading}
+          />
+      </Modal.Actions>
+    </Modal>
+  )
+}
 
 const PostDetail = () => {
   // const params = useParams()
@@ -26,6 +93,8 @@ const PostDetail = () => {
           <p>
             {data.content}
           </p>
+          <Divider />
+          <DeleteModal postSlug={postSlug} title={data.title} thumbnail={data.thumbnail} />
         </div>
       )}
     </Container>
